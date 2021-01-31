b223527fae3608e15c2cc187001290eb1ad4c71b
Paul Hayes <paul.hayes@entropedia.co.uk>
2021-01-31 00:06:47 +0000

    Added fling sfx Improved fling, with supplimentary spherecast.

---
+++ Assets/Scripts/SoundCollection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SoundCollection : ScriptableObject
+{
+    [SerializeField] AudioClip[] sounds;
+
+    int index = 0;
+    public AudioClip NextClip(){
+        if(index>=sounds.Length){
+            index=0;
+        }
+        return sounds[index++];
+    }
+}
---
+++ Assets/Sound/PlaySound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    [SerializeField] SoundCollection soundCollection;
+    [SerializeField] AudioSource audioSource;
+
+    public void Play()
+    {
+        audioSource.PlayOneShot(soundCollection.NextClip());
+    }
+}
