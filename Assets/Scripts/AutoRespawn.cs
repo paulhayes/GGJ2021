8402bc8472e75874e30c91e1fa3dc94e0223ec4f
Paul Hayes <paul.hayes@entropedia.co.uk>
2021-01-31 12:23:42 +0000

    Upgraded menu with new fonts

--- Assets/Scripts/AutoRespawn.cs
+++ Assets/Scripts/AutoRespawn.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class AutoRespawn : MonoBehaviour
 {
     //if below a y position, respawn
+    [SerializeField] float bottom = -10f;
+    [SerializeField] Transform targetTransform;
+    Vector3 startPos;
+    GameObject duplicate;
 
+    void Awake()
+    {
+        gameObject.SetActive(false);
+        duplicate=Instantiate(gameObject,transform.position,transform.rotation);
+        gameObject.SetActive(true);
+    }
     void Update()
     {
-        
+        if(targetTransform.position.y < bottom){
+            duplicate.SetActive(true);
+        }
     }
 }
