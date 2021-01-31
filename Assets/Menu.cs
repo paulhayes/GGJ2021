7b1efcca079bff42b73633467baae0396db227fb
Paul Hayes <paul.hayes@entropedia.co.uk>
2021-01-31 11:47:15 +0000

    Added basic Menu

---
+++ Assets/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] GameObject menuObject;
+    void Awake(){
+        menuObject.SetActive(false);
+
+        if(Application.platform == RuntimePlatform.WebGLPlayer ){
+            GameObject.Find("Quit").SetActive(false); 
+        }
+    }
+
+    void Update()
+    {
+        if( Input.GetKeyDown(KeyCode.Escape)){
+            menuObject.SetActive( !menuObject.activeSelf );
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void Quit(){
+        Application.Quit();
+    }
+}
